20f1fb6f10d79856efa3e426872aab720a3350c4
Masayuki Igawa <masayuki@igawa.me>
2017-06-05 13:50:38 +0900

    Update docs theme
    This commit updates the docs theme to the new one. The new theme is the
    standard of the openstack document. There is no reason to avoid to use
    it.
    
    Change-Id: If17ce35bd2ba50c3e1ae6e1567dcbe4be59d5189

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,8 @@
 import os
 import sys
 
+import openstackdocstheme
+
 sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ----------------------------------------------------
 
@@ -54,8 +56,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-# html_theme_path = ["."]
-# html_theme = '_theme'
+html_theme_path = [openstackdocstheme.get_html_theme_path()]
+html_theme = 'openstackdocs'
 # html_static_path = ['static']
 
 # Output file base name for HTML help builder.
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -3,10 +3,12 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
+========================================================
 Welcome to coverage2sql's documentation!
 ========================================================
 
-Contents:
+Contents
+========
 
 .. toctree::
    :maxdepth: 2
@@ -19,7 +21,4 @@ Contents:
 Indices and tables
 ==================
 
-* :ref:`genindex`
-* :ref:`modindex`
 * :ref:`search`
-
