12b53e108a27178e23d6476eacb0039646b07587
Hervé Beraud <hberaud@redhat.com>
2020-06-02 20:09:13 +0200

    Stop to use the __future__ module.
    The __future__ module [1] was used in this context to ensure compatibility
    between python 2 and python 3.
    
    We previously dropped the support of python 2.7 [2] and now we only support
    python 3 so we don't need to continue to use this module and the imports
    listed below.
    
    Imports commonly used and their related PEPs:
    - `division` is related to PEP 238 [3]
    - `print_function` is related to PEP 3105 [4]
    - `unicode_literals` is related to PEP 3112 [5]
    - `with_statement` is related to PEP 343 [6]
    - `absolute_import` is related to PEP 328 [7]
    
    [1] https://docs.python.org/3/library/__future__.html
    [2] https://governance.openstack.org/tc/goals/selected/ussuri/drop-py27.html
    [3] https://www.python.org/dev/peps/pep-0238
    [4] https://www.python.org/dev/peps/pep-3105
    [5] https://www.python.org/dev/peps/pep-3112
    [6] https://www.python.org/dev/peps/pep-0343
    [7] https://www.python.org/dev/peps/pep-0328
    
    Change-Id: I46f1872407495af231e65cfe784cee731b345f3b

--- coverage2sql/migrations/env.py
+++ coverage2sql/migrations/env.py
@@ -12,8 +12,6 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from __future__ import with_statement
 from alembic import context
 from logging.config import fileConfig
 
