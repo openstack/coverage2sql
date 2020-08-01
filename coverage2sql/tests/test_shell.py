2409d79d62462acd46c35caf01c8888790f31b15
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-08-01 14:44:56 -0500

    Use unittest.mock instead of third party mock
    Now that we no longer support py27, we can use the standard library
    unittest.mock module instead of the third party mock lib.
    
    Change-Id: I4869426a4c39071a9e6d60c02ed13f8e5bb72536

--- coverage2sql/tests/test_shell.py
+++ coverage2sql/tests/test_shell.py
@@ -13,8 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from unittest import mock
+
 import fixtures
-import mock
 from oslo_config import cfg
 import tempfile
 
